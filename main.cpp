599492eb85f7a8f251c8d677d99f25d90c22a623
heruv <136377622+heruv@users.noreply.github.com>
2024-12-20 07:29:23 +0300

    feat: add question task

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "sdt.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "enter a:";
+    cin >> a;
+
+    cout << "enter b:";
+    cin >> b;
+
+    cout << "a: " << a << endl << "b: " << b << endl;
+
+    return 0;
+}
